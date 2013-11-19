d8ee2ebf7c9e7aa74ee9a7756349ac3b848c6172
messageofDEATH <messageofDEATH@ChrisComputer.gateway.2wire.net>
2013-11-18 20:26:07 -0600

    Added a check from the InventoryManager to check if it contains the inventory before checking if the inventory has a Menu.

--- src/net/mcpolitics/dev/Spree/Listener/InventoryListener.java
+++ src/net/mcpolitics/dev/Spree/Listener/InventoryListener.java
@@ -22,25 +22,27 @@ public class InventoryListener implements Listener {
 	@EventHandler
 	public void inventoryClick(InventoryClickEvent event) {
 		if(event.getWhoClicked() instanceof Player) {
-			if(this.instance.getInventoryManager().hasMenu(event.getInventory())) {
-				if(event.getSlot() != -999)/*To prevent null pointer (This is off the inventory screen click)*/ {
-					if(event.getRawSlot() < event.getView().getTopInventory().getSize()) {
-						CustomMenu inv = this.instance.getInventoryManager().getMenu(event.getInventory());
-						if(event.getCurrentItem() != null) {
-							if(event.getCurrentItem().getType() != Material.AIR && event.getCurrentItem() != null) {
-								inv.doAction((Player)event.getWhoClicked(), event.getCurrentItem());
-								event.setCancelled(true);
-							}else{
-								event.setCancelled(true);
+			if(this.instance.getInventoryManager().hasInventory(event.getInventory())) {
+				if(this.instance.getInventoryManager().hasMenu(event.getInventory())) {
+					if(event.getSlot() != -999)/*To prevent null pointer (This is off the inventory screen click)*/ {
+						if(event.getRawSlot() < event.getView().getTopInventory().getSize()) {
+							CustomMenu inv = this.instance.getInventoryManager().getMenu(event.getInventory());
+							if(event.getCurrentItem() != null) {
+								if(event.getCurrentItem().getType() != Material.AIR && event.getCurrentItem() != null) {
+									inv.doAction((Player)event.getWhoClicked(), event.getCurrentItem());
+									event.setCancelled(true);
+								}else{
+									event.setCancelled(true);
+								}
 							}
-						}
-						inv.checkInventory();
-					}else{
-						event.setCancelled(true);
-						Messenger msg = new Messenger(ChatColor.translateAlternateColorCodes('&', "&8[&5&lSpree&8]&6 "));
-						if(event.getWhoClicked() instanceof Player) {
-							msg.sendMessage((Player)event.getWhoClicked(), ChatColor.BLACK + "[" + ChatColor.DARK_RED + "Error" + ChatColor.BLACK + "] " + ChatColor.RED 
-									+ "You cannot edit your inventory while interacting with the Vendor!", false);
+							inv.checkInventory();
+						}else{
+							event.setCancelled(true);
+							Messenger msg = new Messenger(ChatColor.translateAlternateColorCodes('&', "&8[&5&lSpree&8]&6 "));
+							if(event.getWhoClicked() instanceof Player) {
+								msg.sendMessage((Player)event.getWhoClicked(), ChatColor.BLACK + "[" + ChatColor.DARK_RED + "Error" + ChatColor.BLACK + "] " + ChatColor.RED 
+										+ "You cannot edit your inventory while interacting with the Vendor!", false);
+							}
 						}
 					}
 				}
